32d1f6f57e15b80ae66621772c3903bb7aab7b4f
Rhuan Barros <rhuanbarros@gmail.com>
2022-11-12 12:28:37 -0300

    SupabaseService Auth ok

--- Program.cs
+++ Program.cs
@@ -3,21 +3,40 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorWebAssemblySupabaseTemplate;
 using Microsoft.AspNetCore.Components.Authorization;
 using BlazorWebAssemblySupabaseTemplate.Providers;
+using BlazorWebAssemblySupabaseTemplate.Services;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+        builder.RootComponents.Add<App>("#app");
+        builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-// builder.Services.AddHttpClient("BaseHttpClient", httpClient =>
-//     {
-//         httpClient.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
-//     }
-// );
+        // builder.Services.AddHttpClient("BaseHttpClient", httpClient =>
+        //     {
+        //         httpClient.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
+        //     }
+        // );
 
-// Auth
-builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
-builder.Services.AddAuthorizationCore();
+        var url = "https://ybqilfcwesgbkvxmgxpm.supabase.co";
+        var key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlicWlsZmN3ZXNnYmt2eG1neHBtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTcyMjAyNDgsImV4cCI6MTk3Mjc5NjI0OH0.PofQ_rSqpTt6EFt7BTgXwEjYNhanUYWLpExBdFq6t2s";
 
-await builder.Build().RunAsync();
+        builder.Services.AddSingleton(args => new SupabaseService(url, key));
+
+        // Auth
+        builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
+        builder.Services.AddAuthorizationCore();
+
+        var host = builder.Build();
+
+
+        // TODO: I dont know if this line is correct?
+        var supabaseService = host.Services.GetRequiredService<SupabaseService>();
+        
+        await host.RunAsync();
+    }
+}
+// await builder.Build().RunAsync();
