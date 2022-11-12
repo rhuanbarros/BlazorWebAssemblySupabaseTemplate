fdb887943c60e62e7b13e124bac63d259849cd2d
Rhuan Barros <rhuanbarros@gmail.com>
2022-11-12 16:57:58 -0300

    Change login to component

---
+++ Pages/Auth/Login.razor.cs
@@ -0,0 +1,8 @@
+namespace BlazorWebAssemblySupabaseTemplate.Pages.Auth;
+
+public partial class Login
+{
+    protected string email {get; set;}
+    protected string password {get; set;}
+}
+
--- Pages/Auth/LoginPage.razor.cs
+++ Pages/Auth/LoginPage.razor.cs
@@ -2,7 +2,6 @@ namespace BlazorWebAssemblySupabaseTemplate.Pages.Auth;
 
 public partial class LoginPage
 {
-    protected string email {get; set;}
-    protected string password {get; set;}
+    
 }
 
