5de99892a9a951265d3e2486ed2613552797d26a
Rhuan Barros <rhuanbarros@gmail.com>
2022-11-12 18:18:38 -0300

    MudTable ok

--- Pages/Crud/CrudPage.razor.cs
+++ Pages/Crud/CrudPage.razor.cs
@@ -1,5 +1,6 @@
 using BlazorWebAssemblySupabaseTemplate.Dtos;
 using BlazorWebAssemblySupabaseTemplate.Services;
+using MudBlazor;
 
 namespace BlazorWebAssemblySupabaseTemplate.Pages.Crud;
 
@@ -12,11 +13,20 @@ public partial class CrudPage
         await GetTable();
     }
 
-    List<Lista> ListaList = null;
+    private IReadOnlyList<Lista> _listaList { get; set; }
+    private IReadOnlyList<Lista> _listaListFiltered { get; set; }
+    private MudTable<Lista> table;
     protected async Task GetTable()
     {
         Postgrest.Responses.ModeledResponse<Lista> modeledResponse = await SupabaseClient.From<Lista>().Get();
-        ListaList = modeledResponse.Models;
+        _listaList = modeledResponse.Models;
+        _listaListFiltered = modeledResponse.Models;
         await InvokeAsync(StateHasChanged);
     }
+
+    private async void OnSearch(string text)
+    {
+        _listaListFiltered = _listaList.Where(arg => arg.titulo.Contains(text)).ToList();
+    }
+
 }
